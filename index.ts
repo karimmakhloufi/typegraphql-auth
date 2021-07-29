5d76accb2f1e517784fa82d594b6eff3701d06c5
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2021-07-29 09:24:29 +0200

    Custom Role

---
+++ index.ts
@@ -0,0 +1,36 @@
+import "reflect-metadata";
+import { ApolloServer } from "apollo-server";
+import { buildSchema } from "type-graphql";
+
+import { ExampleResolver } from "./resolver";
+import { Context } from "./context.interface";
+import { authChecker } from "./auth-checker";
+
+void (async function bootstrap() {
+  // build TypeGraphQL executable schema
+  const schema = await buildSchema({
+    resolvers: [ExampleResolver],
+    authChecker, // register auth checking function
+  });
+
+  // Create GraphQL server
+  const server = new ApolloServer({
+    schema,
+    context: () => {
+      const ctx: Context = {
+        // create mocked user in context
+        // in real app you would be mapping user from `req.user` or sth
+        user: {
+          id: 1,
+          name: "Sample user",
+          roles: ["ADMIN"],
+        },
+      };
+      return ctx;
+    },
+  });
+
+  // Start the server
+  const { url } = await server.listen(4000);
+  console.log(`Server is running, GraphQL Playground available at ${url}`);
+})();
---
+++ recipe.type.ts
@@ -0,0 +1,23 @@
+import { ObjectType, Field, Int, Authorized, Float } from "type-graphql";
+
+@ObjectType()
+export class Recipe {
+  @Field()
+  title: string;
+
+  @Field({ nullable: true })
+  description?: string;
+
+  @Authorized() // restrict access to ingredients only for logged users (paid subscription?)
+  @Field((type) => [String])
+  ingredients: string[];
+
+  @Authorized("CUSTOM_ADMIN") // restrict access to rates details for admin only
+  @Field((type) => [Int])
+  ratings: number[];
+
+  @Field((type) => Float, { nullable: true })
+  get averageRating(): number | null {
+    return this.ratings.reduce((a, b) => a + b, 0) / this.ratings.length;
+  }
+}
